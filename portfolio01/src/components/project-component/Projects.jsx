9338cd637c8b0a558d7cb6c527c2baae7f065a1e
Baggu516 <bhargavsainallagutla@gmail.com>
2023-12-12 16:23:40 +0530

    projects

--- portfolio01/src/components/project-component/Projects.jsx
+++ portfolio01/src/components/project-component/Projects.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
-
+import React from "react";
+import { projectData } from "./projectData";
+import { Link } from "react-router-dom";
+import "./project.css";
 const Projects = () => {
   return (
     <div>
       <center>
-      No projects Yet...............
+        {/* No projects Yet............... */}
+        <div className="project-container">
+        {projectData.map((item, i) => 
+          <div className="project-card">
+            <img
+              src={`${projectData[i].img1}`}
+              alt=""
+              className="project-img"
+            />
+            <div className="project-card-content">
+              <h3 className="h3">{item.title}</h3>
+              <p className="project-content">
+                {item.desc}
+              </p>
+              <div className="linku">
+                <ion-icon name="arrow-redo-outline" size="small"></ion-icon>
+                <Link to={`${item.link}`} className="l">
+                  Link
+                </Link>
+              </div>
+            </div>
+          </div>
+        )}
+        </div>
       </center>
-     
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
---
+++ portfolio01/src/components/project-component/projectData.js
@@ -0,0 +1,35 @@
+export const projectData=[{
+   title:"Quiz Hub" ,
+   img1:"./images/quizz.png",
+   desc:"Developed a web application Quiz Hub  by using React JS that help users to test their knownledge in various fields by taking quiz.",
+   link:"https://quizhub01.netlify.app/"
+
+},
+{
+    title:"Railway Ticket Booking" ,
+    img1:"./images/train.png",
+    desc:"Developed frontend for Train Ticket Booking System using React and React Material UI, where user can search,select and book tickets based on available trains and destination location",
+    link:"https://trainticketbooking01.netlify.app/"
+ 
+ },
+ {
+    title:"Birthday Remainder" ,
+    img1:"./images/birthday.png",
+    desc:"Developed a web application Birthday Remainder using React and React Material UI that notifies the user regarding their birthday when user opens the application.",
+    link:"htttcxnm"
+ 
+ },
+ {
+    title:"ToDo List" ,
+    img1:"./images/todo.png",
+    desc:"Developed a web application that helps user to keep of list of activities to do, using React js and Typescript.",
+    link:"https://typescripttodo01.netlify.app/"
+ 
+ },
+ {
+    title:"Photo-Graphy website" ,
+    img1:"./images/photography.png",
+    desc:"Developed a web application Birthday Remainder using React and React Material UI which allows anyone to showcase their photography skills by uploading photos.",
+    link:"https://photography01.netlify.app/"
+ 
+ }]
